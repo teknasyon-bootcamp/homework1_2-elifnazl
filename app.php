6b87859b05c8a30972f411f6c9e9f650e03bb9ef
elifnazl <86831639+elifnazl@users.noreply.github.com>
2021-08-20 03:03:50 +0300

    Update app.php

--- app.php
+++ app.php
@@ -19,30 +19,6 @@ if ($guess == $BMR) {
 } else {
 	echo "tahmin degerinden buyuk";
 }
-/**
- * Bu ödevde bazal metabolizma hesaplaması yapacağız.
- * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
- * kişinin günlük BMR ihtiyacını hesaplayıp
- * bunu ekrana yazan ve tahmin değeri ile karşılaştıran
- * PHP kodunu oluşturmamız gerekiyor.
- * 
- * Erkekler için; 88.362 + (13.397 x kilo) + (4.799 x boy) – (5.677 x yaş)
- * Kadınlar için; 447.593 + (9.247 x kilo) + (3.098 x boy) – (4.330 x yaş)
- * 
- * Tahmin değeri ile karşılaştırmanızın sonucunda
- * "Tahmin değerinden düşük", "Tahmin değerinden büyük" veya
- * "Tahmin değerine eşit" şeklinde çıktı vermeniz gerekiyor.
- * 
- * Örneğin;
- * $gender = "female";
- * $weight = 60;
- * $height = 170;
- * $age = 30;
- * 
- * Ekrana
- * 
- * BMR: 1399.173
- * Tahmin değerinden düşük
- * 
- * yazması gerekiyor.
- */
+
+/*   yukarıdaki degerler icin ekran ciktisi 
+          BMR:1499.102tahmin degerinden buyuk      */
