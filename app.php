63b73627a26361408e32fcf4603ea5f41a50d84b
elifnazl <86831639+elifnazl@users.noreply.github.com>
2021-08-20 03:01:20 +0300

    Update app.php

--- app.php
+++ app.php
@@ -4,9 +4,21 @@ $gender = "male"; // cinsiyet (male/female)
 $weight = 55; // kilo (kg)
 $height = 170; // boy (cm)
 $age = 25; // yaş (sene)
-
 $guess = "1470"; // Tahmin edilen değer
 
+if ($gender=="male"){
+  	$BMR = 88.362 + (13.397*$weight) + (4.799*$height) - (5.677*$age);
+}else {
+    $BMR = 447.593+(9.247*$weight)+(3.098*$height)-(4.330*$age);
+}
+echo "BMR:".$BMR;
+if ($guess == $BMR) {
+	echo "tahmin degerine esit";
+} elseif ($guess > $BMR) {
+	echo "tahmin degerinden kucuk";
+} else {
+	echo "tahmin degerinden buyuk";
+}
 /**
  * Bu ödevde bazal metabolizma hesaplaması yapacağız.
  * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
